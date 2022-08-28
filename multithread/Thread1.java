6631df2f26259350b31e4150b0782639701c45f1
liyubo-shy <859591603@qq.com>
2022-08-28 10:39:07 +0800

    e

--- multithread/Thread1.java
+++ multithread/Thread1.java
@@ -9,7 +9,29 @@ package multithread;
  */
 public class Thread1 {
     public static void main(String[] args) {
-        Thread thr1 = new Thread();
+        Thread thr1 = new MyThread1();
+        Thread thr2 = new MyThread2();
+        thr1.start();
+        thr2.start();
 
+    }
+}
+
+class MyThread1 extends Thread{
+    @Override
+    public void run() {
+        for (int i = 0; i <= 10; i++){
+            System.out.println("1号线程："+i);
+        }
+    }
+}
+
+class MyThread2 extends Thread{
+    @Override
+    public void run() {
+        for (int i = 0; i <= 10; i++){
+            System.out.println("2号线程："+i);
+
+        }
     }
 }
--- multithread/Thread2.java
+++ multithread/Thread2.java
@@ -9,6 +9,12 @@ package multithread;
  */
 public class Thread2 {
     public static void main(String[] args) {
-
+        System.out.println("main start...");
+        Thread t = new Thread(() -> {
+            System.out.println("thread run...");
+            System.out.println("thread end.");
+        });
+        t.start();
+        System.out.println("main end...");
     }
 }
