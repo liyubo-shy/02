3fc731e8315e78a13ebf10ca097e8247db552431
liyubo-shy <859591603@qq.com>
2022-09-02 05:51:28 +0800

    0902 无关紧要的修改

--- Count.java
+++ Count.java
@@ -7,7 +7,7 @@
  */
 public class Count {
     public static void main(String[] args) {
-        double a = 11480140;//available
+        double a = 11482532;//available
         double b = 16247604;//total
         System.out.println((1 - (a/b)) * 100);
     }
