ff7943d74639025e2f7ef582014708cef90cee29
liyubo-shy <859591603@qq.com>
2022-09-05 21:53:27 +0800

    0905 IO——图片复制

--- Count.java
+++ Count.java
@@ -7,7 +7,7 @@
  */
 public class Count {
     public static void main(String[] args) {
-        double a = 11459908;//available
+        double a = 11464080;//available
         double b = 16247604;//total
         System.out.println((1 - (a/b)) * 100);
     }
---
+++ basictype/io/CopyImg.java
@@ -0,0 +1,55 @@
+package basictype.io;
+
+import java.io.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * 复制图片，并输出文件大小
+ *
+ * @Author: bo
+ * @Date: 2022/09/05/21:20
+ * @Description: 在人间已是癫，何苦要上青天，不如温柔同眠
+ */
+public class CopyImg {
+    public static void main(String[] args) {
+        FileInputStream fileIn = null;
+        FileOutputStream filOut = null;
+        //定义路径
+        File filePath = new File("D:\\小照片\\微信图片_20220905003714(1).png");
+        File copyPath = new File("D:\\小照片\\newImg.png");
+        //定义int计算文件大小
+        int size = 0;
+
+        try {
+            fileIn = new FileInputStream(filePath);
+            filOut = new FileOutputStream(copyPath, true);
+            //定义字节数组，一次读取1024字节的数据
+            byte[] buf = new byte[1024];
+            //接收read方法返回的字节长度
+            int len = 0;
+            //开始复制
+            while ((len = fileIn.read(buf)) != -1) {
+                //读取到的字节数组写入新文件
+                filOut.write(buf, 0, len);
+                //统计总字节长度
+                size += len;
+            }
+            System.out.println("文件大小：" + size / 1024 / 1024 + "M");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //关闭资源
+            try {
+                if (fileIn != null) {
+                    fileIn.close();
+                }
+                if (filOut != null) {
+                    filOut.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
